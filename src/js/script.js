f2596a500da05281d1fff9f4538cf477bfc3bf94
Diego Rocha <diegoarocha1369@gmail.com>
2026-04-06 10:43:16 -0300

    feat(script.js): alert do cadastro adicionado

--- src/js/script.js
+++ src/js/script.js
@@ -3,3 +3,5 @@ let tipoVinho = prompt("Digite o tipo do vinho: Tinto, Branco, Rosé");
 let safraVinho = prompt("Digite a safra do vinho:");
 let quantidadeVinho = parseInt(prompt("Digite a quantidade de garrafas no estoque:"));
 let precoVinho = parseFloat(prompt("Digite o preço do vinho:"));
+
+alert("O Cadastro foi realizado com sucesso! Veja mais detalhes no console.");
